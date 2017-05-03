c72ceef86e4cb51c6654ae69bd4297a1d7ca6acb
Prokop Simek <prokop.simek@applifting.cz>
2017-05-03 09:19:38 +0200

    change function position

--- src/reducers/sortOrdering.ts
+++ src/reducers/sortOrdering.ts
@@ -3,44 +3,15 @@ import { SORT_TODOS } from '../constants/actionTypes';
 
 
 
-const sortTodos = (todos, order) => {
-  return todos.sort(
-    (todo_prev, todo_next) => {   
-      const todo_prev_priority = parseInt(todo_prev.priority);
-      const todo_next_priority = parseInt(todo_next.priority);
-      console.warn(todo_prev_priority);
-      
-      if (order == 'ASC') {
-          if ( todo_prev_priority < todo_next_priority ) {
-            return -1;
-          }
-          if ( todo_prev_priority > todo_next_priority ) {
-            return 1;
-          }
-          return 0;
-        
-    } else {
-          if ( todo_prev_priority < todo_next_priority ) {
-            return 1;
-          }
-          if ( todo_prev_priority > todo_next_priority ) {
-            return -1;
-          }
-          return 0;
-        }
-    }
-  )
-
-}
 
 
 
-const sortOrdering = (state: Todo[] = [], action: any) => {
+
+const sortOrdering = (state: String = "ASC", action: any) => {
   switch (action.type) {
     case SORT_TODOS:
       console.warn(state);
-      console.warn(sortTodos(state, action.order));
-      return sortTodos(state, action.order);
+      return action.order;//sortTodos(state, action.order);
     default:
       return state;
   }
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -18,12 +18,44 @@ const todo = (state: Todo, action: any) => {
       }
       return Object.assign({}, state, {
         priority: action.priority
-      });     
+      });
+
       
     default:
       return state;
   }
 };
+
+
+const sortTodos = (todos, order) => {
+  return todos.sort(
+    (todo_prev, todo_next) => {
+      const todo_prev_priority = parseInt(todo_prev.priority);
+      const todo_next_priority = parseInt(todo_next.priority);
+      console.warn(todo_prev_priority);
+
+      if (order === 'ASC') {
+          if ( todo_prev_priority < todo_next_priority ) {
+            return -1;
+          }
+          if ( todo_prev_priority > todo_next_priority ) {
+            return 1;
+          }
+          return 0;
+
+    } else {
+          if ( todo_prev_priority < todo_next_priority ) {
+            return 1;
+          }
+          if ( todo_prev_priority > todo_next_priority ) {
+            return -1;
+          }
+          return 0;
+        }
+    }
+  )
+
+}
 
 const todos = (state: Todo[] = [], action: any) => {
   switch (action.type) {
@@ -42,23 +74,23 @@ const todos = (state: Todo[] = [], action: any) => {
       ];
     case TOGGLE_TODO:
       return state.map(t => todo(t, action));
-    
+
     case DELETE_TODO:
-      return state.filter(t => t.id != action.id);
+      return state.filter(t => t.id !== action.id);
 
     case SET_PRIORITY:
       return state.map(t => todo(t, action));
-    
+
     default:
       return state;
   }
 };
 
 
-function getLastId(todos: Todo[]):number{
+function getLastId(todos: Todo[]): number{
   let id = 1;
   todos.forEach(todo => {
-    if(todo.id > id){
+    if (todo.id > id){
       id = todo.id;
     }
   });
